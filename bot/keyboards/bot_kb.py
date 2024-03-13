76e6172ae7d97ac816d23d68bf50451c201ede51
AlexanderCruciata <AlexanderCruciata1012@gmail.com>
2024-03-13 05:01:03 -0400

    finish

--- bot/keyboards/bot_kb.py
+++ bot/keyboards/bot_kb.py
@@ -1,9 +1,40 @@
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+import requests
+
+
 class Keyboards:
     @staticmethod
     async def user_keyboard():
         builder = ReplyKeyboardBuilder()
         builder.button(text="/books_list")
         builder.button(text="/random_book")
+        builder.button(text="/books_by_category")
+        builder.button(text="/books")
+        builder.adjust(1)
+        return builder.as_markup()
+
+    @staticmethod
+    async def books_by_category():
+        response = requests.get("http://127.0.0.1:8000/api/books-categories/")
+        builder = InlineKeyboardBuilder()
+        if response.status_code == 200:
+            response = response.json()
+            for category in response:
+                category_name = category["category_name"]
+                builder.button(
+                    text=category_name, callback_data=f"category_{category_name}"
+                )
+        builder.adjust(2)
+        return builder.as_markup()
+
+    @staticmethod
+    async def books():
+        response = requests.get("http://127.0.0.1:8000/api/books-list/")
+        builder = InlineKeyboardBuilder()
+        if response.status_code == 200:
+            response = response.json()
+            for book in response:
+                book_name = book["name"]
+                builder.button(text=book_name, callback_data=f"book_{book_name}")
         builder.adjust(1)
         return builder.as_markup()
--- bot/routers/bot_routers.py
+++ bot/routers/bot_routers.py
@@ -1,13 +1,11 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import FSInputFile, InputFile, Message
-from aiogram.utils.media_group import MediaGroupBuilder
+from aiogram.types import CallbackQuery, FSInputFile, Message
 from keyboards.bot_kb import Keyboards  # pyright:ignore
-from create_bot import BOT_PATH, bot  # pyright:ignore
+
 import requests
 import os
 
-# дjбавить в модели джанго чтобы в image поле были абсолютные пути
 router = Router()
 keyboards = Keyboards()
 
@@ -25,9 +23,8 @@ async def command_books_list(message: Message):
     response = requests.get("http://127.0.0.1:8000/api/books-list/")
     if response.status_code == 200:
         for book in response.json():
-            await message.answer(
-                f"Name:{book['name']}.Description:{book['description']}"
-            )
+            print(book)
+            await message.answer(book["name"])
 
 
 @router.message(Command("random_book"))
@@ -36,5 +33,65 @@ async def command_random_book(message: Message):
     if response.status_code == 200:
         book = response.json()[0]
         book_image = book["image"]
-        file = FSInputFile(os.path.join(os.getcwd()[:-4],"admin_panel","books_images",book_image[14:]),filename=book_image[14:])
-        await message.answer_photo(photo=file,caption="caption")
+        file = FSInputFile(
+            os.path.join(
+                os.getcwd()[:-4], "admin_panel", "books_images", book_image[14:]
+            ),
+            filename=book_image[14:],
+        )
+        await message.answer_photo(
+            photo=file, caption=f"Name:{book['name']}.Description:{book['description']}"
+        )
+
+
+@router.message(Command("books_by_category"))
+async def command_books_by_category(message: Message):
+    await message.answer(
+        "Choose genre", reply_markup=await keyboards.books_by_category()
+    )
+
+
+@router.message(Command("books"))
+async def command_books(message: Message):
+    await message.answer("Choose book", reply_markup=await keyboards.books())
+
+
+@router.callback_query(F.data.startswith("category_"))
+async def get_book_by_category(callback_query: CallbackQuery):
+    category_name = callback_query.data[9:]
+    response = requests.get(
+        f"http://127.0.0.1:8000/api/book-by-category/{category_name}"
+    )
+    if response.status_code == 200:
+        books = response.json()
+        for book in books:
+            book_image = book["image"]
+            file = FSInputFile(
+                os.path.join(
+                    os.getcwd()[:-4], "admin_panel", "books_images", book_image[14:]
+                ),
+                filename=book_image[14:],
+            )
+            await callback_query.message.answer_photo(
+                photo=file,
+                caption=f"Name:{book['name']}.Description:{book['description']}",
+            )
+
+
+@router.callback_query(F.data.startswith("book_"))
+async def get_book_by_name(callback_query: CallbackQuery):
+    book_name = callback_query.data[5:]
+    response = requests.get(f"http://127.0.0.1:8000/api/book-by-name/{book_name}")
+    if response.status_code == 200:
+        book = response.json()[0]
+        book_image = book["image"]
+        file = FSInputFile(
+            os.path.join(
+                os.getcwd()[:-4], "admin_panel", "books_images", book_image[14:]
+            ),
+            filename=book_image[14:],
+        )
+        await callback_query.message.answer_photo(
+            photo=file,
+            caption=f"Name:{book['name']}.Description:{book['description']}",
+        )
